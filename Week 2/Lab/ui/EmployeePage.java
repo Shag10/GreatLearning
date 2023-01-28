9a37b7dd992cc5b2cd97d7a3964cd93f635a843f
Shubhang Agrawal <shubhangagrawal1999@gmail.com>
2023-01-28 21:22:13 +0530

    Add files via upload

---
+++ Week 2/Lab/ui/EmployeePage.java
@@ -0,0 +1,34 @@
+package BankingSystem.ui;
+
+import java.util.List;
+import java.util.Scanner;
+
+import BankingSystem.dao.PersonDao;
+import BankingSystem.pojos.Person;
+
+public class EmployeePage {
+	public static void employeePage(PersonDao personDao) {
+		Scanner sc=new Scanner(System.in);
+		int ch=0;
+		while(ch!=2) {
+			System.out.println("1: show all employees details: ");
+			System.out.println("2: logout: ");
+			System.out.println("enter your choice");
+			ch=sc.nextInt();
+			if(ch==1) {
+				List<Person>persons=personDao.getPersons();
+				for(int i=0;i<persons.size();i++) {
+					if(persons.get(i).getType().equalsIgnoreCase("employee"))
+					System.out.println(persons.get(i).toString());
+				}
+				
+			}else {
+				System.out.println("logged out sucessfully");
+			}
+			
+		}
+		sc.close();
+	}
+
+}
+
