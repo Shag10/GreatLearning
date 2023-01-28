681e0e8829094811c69dd48121f9a417f5a58516
Shubhang Agrawal <shubhangagrawal1999@gmail.com>
2023-01-28 21:19:01 +0530

    Add files via upload

---
+++ Week 2/Lab/dao/AccountDao.java
@@ -0,0 +1,41 @@
+package BankingSystem.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import BankingSystem.pojos.Account;
+import BankingSystem.pojos.CurrentAccount;
+import BankingSystem.pojos.Person;
+import BankingSystem.pojos.SavingsAccount;
+
+public class AccountDao {
+	List<Account> accounts;
+
+	public AccountDao() {
+		this.accounts=new ArrayList<>();
+	}
+	
+	public void createAcoount(Person person,String accountNumber,String type) {
+		if(type.equalsIgnoreCase("savings")) {
+			Account account=new SavingsAccount(accountNumber, 0, person);
+			accounts.add(account);
+			System.out.println("Created savings account seccessfully");
+		}else if(type.equalsIgnoreCase("current")){
+			Account account=new CurrentAccount(accountNumber, 0, person);
+			accounts.add(account);
+			System.out.println("Created current account seccessfully");
+			
+		}
+		
+	}
+	public Account findAccount(Person person,String number) {
+		for(int i=0;i<accounts.size();i++) {
+			if(accounts.get(i).getAccountNumber().equalsIgnoreCase(number)&&accounts.get(i).getCustomer().equals(person)) {
+				return accounts.get(i);
+			};
+			
+		}
+		return null;
+	}	
+
+}
+
