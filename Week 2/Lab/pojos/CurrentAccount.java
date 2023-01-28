f7cf79cf2286263520949a648351b493ccfc7e01
Shubhang Agrawal <shubhangagrawal1999@gmail.com>
2023-01-28 21:21:03 +0530

    Add files via upload

---
+++ Week 2/Lab/pojos/CurrentAccount.java
@@ -0,0 +1,10 @@
+package BankingSystem.pojos;
+
+public class CurrentAccount extends Account {
+
+	public CurrentAccount(String accountNumber, double balance,Person customer) {
+		super(accountNumber, balance,"Current Account",customer);
+		// TODO Auto-generated constructor stub
+	}
+
+}
