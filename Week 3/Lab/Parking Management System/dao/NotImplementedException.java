7b04d5847d75f1d726f84296c4b9f46ac5adfe0a
Shubhang Agrawal <shubhangagrawal1999@gmail.com>
2023-03-13 02:36:51 +0530

    Add files via upload

---
+++ Week 3/Lab/Parking Management System/dao/NotImplementedException.java
@@ -0,0 +1,15 @@
+package dao;
+
+public class NotImplementedException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public NotImplementedException(String message)
+	{
+		super(message);
+		
+	}
+
+}
